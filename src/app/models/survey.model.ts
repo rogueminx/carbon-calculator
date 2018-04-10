6a623e14acbb31b7fb67d3d22c79ff4b8428ea54
russell hofvendahl <rhofvendahl@gmail.com>
2018-04-10 15:15:39 -0700

    update model

--- src/app/models/survey.model.ts
+++ src/app/models/survey.model.ts
@@ -31,3 +31,43 @@ export class Survey {
     return baseline + foodModifier + housingModifier + transportModifier;
   }
 }
+
+// export class Survey {
+//   date: string = new Date().toISOString().split("T")[0];
+//   userId: string;
+//
+//   animalProductCo2: number;
+//   foodCo2: number;
+//
+//   squareFeet: number;
+//   peopleInHome: number;
+//   housingCo2: number;
+//
+//   energyCo2: number;
+//
+//   trashCo2: number;
+//
+//   vehicleMiles: number;
+//   milesPerGallon: number;
+//   publicMiles: number;
+//   hoursFlown: number;
+//   transportCo2: number;
+//
+//   totalCo2: number;
+//
+//   constructor() {}
+//
+//   calculate() {
+//     this.foodCo2 = 2 + this.animalProductCo2;
+//
+//     let squareFeetPerPerson = this.squareFeet / this.peopleInHome;
+//     this.housingCo2 = squareFeetPerPerson*3/5000;
+//
+//     let gallonsPerWeek = this.vehicleMiles / this.milesPerGallon;
+//     this.transportCo2 = gallonsPerWeek*.4 + this.publicMiles*.01 + this.hoursFlown*.1;
+//
+//     this.totalCo2 = foodCo2 + housingCo2 + transportCo2;
+//   }
+//
+//
+// }
