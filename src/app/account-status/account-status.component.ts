9c32c92dc832f9f2f03d61f2dfed5ed73e92647c
Anna Marie Cooper and Russell Hofvendahl <russell@>
2018-04-10 14:33:59 -0700

    google login now saves user to database.

--- src/app/account-status/account-status.component.ts
+++ src/app/account-status/account-status.component.ts
@@ -10,24 +10,14 @@ import { AuthenticationService } from '../services/authentication.service';
 export class AccountStatusComponent implements OnInit {
   public user;
   private isLoggedIn: boolean;
+
   constructor(public authService: AuthenticationService) { }
 
   ngOnInit() {
-<<<<<<< HEAD
-    this.authService.user.subscribe(user => {
-      if (user == null) {
-        this.isLoggedIn = false;
-      } else {
-        this.isLoggedIn = true;
-        this.user = user
-      }
-    });
-=======
     this.authService.user.subscribe(user=> {
       this.user = user;
       console.log(user)
     })
->>>>>>> 6b52fd719e5706b66e0f2b1849452132690bc1e0
   }
 
   logout() {
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,18 +2,33 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from '../models/user.model';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthenticationService {
-
+  authenticatedUserUID: string;
   user: Observable<firebase.User>
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, public database: AngularFireDatabase) {
     this.user = afAuth.authState;
   }
 
   loginWithGoogle() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then(signedInUser => {
+      if(signedInUser) {
+        let uid = firebase.auth().currentUser.uid;
+        this.authenticatedUserUID = uid;
+        this.userExists(uid).subscribe(user => {
+          if (!user) {
+            const newUser = new User (signedInUser.user.displayName, signedInUser.user.uid, signedInUser.user.email);
+            this.pushUserToDatabase(newUser);
+          }
+        })
+      }
+    });
   }
 
   logout() {
@@ -21,11 +36,26 @@ export class AuthenticationService {
   }
 
   createUser(email, password, displayName) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(user => {
-      user.sendEmailVerification();
-      user.updateProfile({displayName:displayName});
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(signedInUser => {
+        signedInUser.sendEmailVerification();
+        signedInUser.updateProfile({displayName:displayName});
+        if(signedInUser) {
+          let uid = firebase.auth().currentUser.uid;
+          this.authenticatedUserUID = uid;
+          this.userExists(uid).subscribe(user => {
+          if (!user) {
+            const newUser = new User (signedInUser.user.displayName, signedInUser.user.uid, signedInUser.user.email);
+            this.pushUserToDatabase(newUser);
+          }
+        })
+      }
     });
+
   }
+
+  pushUserToDatabase(newUser: User): void {
+    this.database.list(`users`).push(newUser);
+}
 
   loginWithEmail(email, password) {
     this.afAuth.auth.signInWithEmailAndPassword(email, password);
@@ -34,4 +64,12 @@ export class AuthenticationService {
   deleteAccount() {
     firebase.auth().currentUser.delete();
   }
+
+  userExists(uid: string): Observable<boolean> {
+    return this.getUserByUID(uid).map(data => !!data[0]);
+   }
+
+   getUserByUID(userUID: string): FirebaseListObservable<any[]> {
+     return this.database.list(`users`, {query: {orderByChild: 'uid', equalTo: userUID}});
+   }
 }
