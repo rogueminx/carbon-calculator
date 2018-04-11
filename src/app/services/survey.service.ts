0e5d0da1005a14f9635e58031d09951128823b33
Cameron Anderson and Kayla Ondracek <kayla@>
2018-04-11 10:02:06 -0700

    list surveys by UID on the user profile

--- src/app/services/survey.service.ts
+++ src/app/services/survey.service.ts
@@ -6,17 +6,21 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class SurveyService {
   surveys: FirebaseListObservable<any[]>
+  userSurveys: FirebaseListObservable<any[]>
 
   constructor(private database: AngularFireDatabase) {
     //this will need to be changed to be per user
   this.surveys = database.list('surveys');
+
  }
 
-//take user or useriD?
   saveSurvey(newSurvey: Survey, user: firebase.User){
-    //this will need to be saved to be per user
     console.log(user.uid);
     this.database.list(`surveys/${user.uid}`).push(newSurvey);
+  }
+
+  getSurveysByUID(user: firebase.User) {
+    return this.database.list(`surveys/${user.uid}`);
   }
 
 }
